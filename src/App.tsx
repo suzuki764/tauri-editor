9067179f93a8b4e89ebda165911ea5f52604903f
Kosuke Suzuki <k.suzuki764@gmail.com>
2023-04-19 14:03:49 +0900

    markdown対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { writeTextFile, BaseDirectory } from "@tauri-apps/api/fs";
+import ReactMarkdown from "react-markdown";
 import "./App.css";
 
 function App() {
   const [contents, setContents] = useState("");
   const [filename, setFilename] = useState("");
   const [message, setMessage] = useState("");
+  const [preview, setPreview] = useState(false);
+
+  const previewHandler = () => {
+    setPreview((prev) => !prev);
+  };
 
   const saveHandler = async () => {
     if (filename === "") {
@@ -19,12 +25,17 @@ function App() {
 
   return (
     <div className="main">
-      <textarea
-        value={contents}
-        onChange={(e) => {
-          setContents(e.target.value);
-        }}
-      />
+      {preview ? (
+        <ReactMarkdown className="editor-area" children={contents} />
+      ) : (
+        <textarea
+          className="editor-area"
+          value={contents}
+          onChange={(e) => {
+            setContents(e.target.value);
+          }}
+        />
+      )}
       <div className="footer">
         <div>{countChars(contents)}文字</div>
         <input
@@ -33,9 +44,13 @@ function App() {
           onChange={(e) => setFilename(e.target.value)}
           className="filename-input"
         />
-        <button className="save-button" onClick={() => saveHandler()}>
-          Save
+        <button
+          className={preview ? "button-on" : ""}
+          onClick={() => previewHandler()}
+        >
+          Preview
         </button>
+        <button onClick={() => saveHandler()}>Save</button>
         <div className="message">{message}</div>
       </div>
     </div>
