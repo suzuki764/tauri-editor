7d9bdd816ecab67f17704da2c9d03c0f7d223047
Kosuke Suzuki <k.suzuki764@gmail.com>
2023-04-19 13:20:37 +0900

    保存機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { writeTextFile, BaseDirectory } from "@tauri-apps/api/fs";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
+  const [contents, setContents] = useState("");
+  const [filename, setFilename] = useState("");
+  const [message, setMessage] = useState("");
+
+  const saveHandler = async () => {
+    if (filename === "") {
+      setMessage("ファイル名を入力してください");
+      return;
+    }
+    const saveName = filename.endsWith(".txt") ? filename : `${filename}.txt`;
+    await writeTextFile(saveName, contents, { dir: BaseDirectory.Desktop });
+    setMessage("保存しました");
+  };
 
   return (
     <div className="main">
       <textarea
+        value={contents}
         onChange={(e) => {
-          setText(e.target.value);
+          setContents(e.target.value);
         }}
       />
-      <div className="info-text">{countChars(text)}文字</div>
+      <div className="footer">
+        <div>{countChars(contents)}文字</div>
+        <input
+          value={filename}
+          placeholder="ファイル名"
+          onChange={(e) => setFilename(e.target.value)}
+          className="filename-input"
+        />
+        <button className="save-button" onClick={() => saveHandler()}>
+          Save
+        </button>
+        <div className="message">{message}</div>
+      </div>
     </div>
   );
 }
